5bc6e5f73a8ba8dad5574b8b9994004541a55c7d
Mahdi <mahdighadiriafzal@gmail.com>
2025-07-18 23:38:08 +0330

    add alert models

--- bizguard/src/apps/alerts/models.py
+++ bizguard/src/apps/alerts/models.py
@@ -5,7 +5,7 @@ from django.db import models
 from apps.accounts.models import User
 from apps.monitoring.models import Website
 from utils.base_model import BaseModel
-from utils.choices import TriggerTypesChoices
+from utils.choices import AlertSeverityChoices, AlertStatusChoices, TriggerTypesChoices
 
 
 class AlertRule(BaseModel):
@@ -27,4 +27,40 @@ class AlertRule(BaseModel):
         indexes = [
             models.Index(fields=["user", "is_active"]),
             models.Index(fields=["website", "trigger_types"]),
+        ]
+
+
+class Alert(BaseModel):
+    """Generated alerts"""
+
+    alert_severity = models.CharField(
+        max_length=55, choices=AlertSeverityChoices.choices
+    )
+    alert_status = models.CharField(max_length=55, choices=AlertStatusChoices.choices)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="alerts")
+    website = models.ForeignKey(
+        Website, on_delete=models.CASCADE, related_name="alerts"
+    )
+    alert_rule = models.ForeignKey(
+        AlertRule, on_delete=models.CASCADE, related_name="alerts"
+    )
+
+    title = models.CharField(max_length=100)
+    message = models.TextField()
+
+    sent_at = models.DateTimeField(null=True, blank=True)
+    acknowledged_at = models.DateTimeField(null=True, blank=True)
+    resolved_at = models.DateTimeField(null=True, blank=True)
+
+    # Metadata
+    trigger_data = models.JSONField(default=dict)
+    delivery_attempts = models.IntegerField(default=0)
+
+    class Meta:
+        db_table = "alerts"
+        indexes = [
+            models.Index(fields=["user", "alert_status"]),
+            models.Index(fields=["website", "created_at"]),
+            models.Index(fields=["alert_severity", "alert_status"]),
         ]
