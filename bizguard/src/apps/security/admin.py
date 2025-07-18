333d00f231d7a7d8dece28b1842b0ff41e6720f8
Mahdi <mahdighadiriafzal@gmail.com>
2025-07-18 21:01:59 +0330

    add admin panel for security app

--- bizguard/src/apps/security/admin.py
+++ bizguard/src/apps/security/admin.py
@@ -1,1 +1,37 @@
-# Register your models here.
+from django.contrib import admin
+
+from .models import SecurityScan, SSLCertificates
+
+
+@admin.register(SecurityScan)
+class SecurityScanAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "website",
+        "scan_types",
+        "severity_levels",
+        "title",
+        "is_resolved",
+        "scanned_at",
+        "resolved_at",
+    )
+    search_fields = ("website__name", "title", "scan_types", "severity_levels")
+    list_filter = ("scan_types", "severity_levels", "is_resolved")
+
+
+@admin.register(SSLCertificates)
+class SSLCertificatesAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "website",
+        "issuer",
+        "subject",
+        "serial_number",
+        "valid_from",
+        "valid_until",
+        "is_valid",
+        "signature_algorithm",
+        "last_checked",
+    )
+    search_fields = ("website__name", "issuer", "subject", "serial_number")
+    list_filter = ("is_valid", "valid_from", "valid_until")
