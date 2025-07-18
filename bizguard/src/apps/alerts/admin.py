0ce593ab9fc0340dbc3feaf242e4654df01f1cf3
Mahdi <mahdighadiriafzal@gmail.com>
2025-07-18 23:46:02 +0330

    add admin panel for alerts app

--- bizguard/src/apps/alerts/admin.py
+++ bizguard/src/apps/alerts/admin.py
@@ -1,1 +1,38 @@
-# Register your models here.
+from django.contrib import admin
+
+from .models import Alert, AlertRule
+
+
+@admin.register(AlertRule)
+class AlertRuleAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "user",
+        "website",
+        "trigger_types",
+        "threshold_value",
+        "is_active",
+        "cooldown_period",
+    )
+    list_filter = ("is_active", "trigger_types", "website", "user")
+    search_fields = ("id", "user__username", "website__name")
+    ordering = ("-created_at",)
+
+
+@admin.register(Alert)
+class AlertAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "user",
+        "website",
+        "alert_rule",
+        "alert_severity",
+        "alert_status",
+        "title",
+        "sent_at",
+        "acknowledged_at",
+        "resolved_at",
+    )
+    list_filter = ("alert_severity", "alert_status", "website", "user")
+    search_fields = ("id", "user__username", "website__name", "title")
+    ordering = ("-created_at",)
