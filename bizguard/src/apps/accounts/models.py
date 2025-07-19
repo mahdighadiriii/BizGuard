858e8fc3019e2a22913eeb3678855b5fa66cdb84
Mahdi <mahdighadiriafzal@gmail.com>
2025-07-19 18:04:53 +0330

    solved phone nuber issue

--- bizguard/src/apps/accounts/models.py
+++ bizguard/src/apps/accounts/models.py
@@ -14,9 +14,9 @@ class User(AbstractUser, BaseModel):
     email = models.EmailField(_("email address"), unique=True)
     first_name = models.CharField(_("first name"), blank=True, null=True)
     last_name = models.CharField(_("last name"), blank=True, null=True)
-    telegram_user_id = models.CharField(max_length=255, blank=False)
+    telegram_user_id = models.BigIntegerField(unique=True, max_length=255, blank=False)
     telegram_username = models.CharField(max_length=255, blank=False)
-    phonenumber = models.CharField(
+    phone_number = models.CharField(
         max_length=15,
         validators=[RegexValidator(r"^\+?1?\d{9,15}$")],
     )
