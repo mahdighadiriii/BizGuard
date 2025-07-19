f99f183a11c926df853e82a43143ab1d81d62cb9
Mahdi <mahdighadiriafzal@gmail.com>
2025-07-19 15:32:42 +0330

    create admin panel for payments app

--- bizguard/src/apps/payments/admin.py
+++ bizguard/src/apps/payments/admin.py
@@ -1,1 +1,35 @@
-# Register your models here.
+from django.contrib import admin
+
+from .models import Payment, Subscription
+
+
+@admin.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "user",
+        "plan",
+        "status",
+        "start_date",
+        "end_date",
+        "auto_renew",
+    )
+    search_fields = ("user__username", "plan", "status")
+    list_filter = ("plan", "status", "auto_renew")
+
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "user",
+        "subscription",
+        "amount",
+        "currency",
+        "status",
+        "gateway",
+        "transaction_id",
+        "completed_at",
+    )
+    search_fields = ("user__username", "transaction_id", "gateway_transaction_id")
+    list_filter = ("status", "gateway", "currency")
