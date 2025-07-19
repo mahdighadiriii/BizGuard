b75328fe4ce0e83f361c3acc052ef3f236779d99
Mahdi <mahdighadiriafzal@gmail.com>
2025-07-19 10:21:37 +0330

    add telegram message model

--- bizguard/src/apps/telegram/models.py
+++ bizguard/src/apps/telegram/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 from apps.accounts.models import User
 from utils.base_model import BaseModel
+from utils.choices import TelegramMessageTypesChoices
 
 
 class TelegramSession(BaseModel):
@@ -17,3 +18,24 @@ class TelegramSession(BaseModel):
 
     class Meta:
         db_table = "telegram_sessions"
+
+
+class TelegramMessages(BaseModel):
+    """Telegram message log"""
+
+    message_type = models.CharField(
+        max_length=55, choices=TelegramMessageTypesChoices.choices
+    )
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="telegram_messages"
+    )
+    content = models.TextField()
+    telegram_message_id = models.PositiveBigIntegerField(null=True, blank=True)
+    is_delivered = models.BooleanField(default=False)
+
+    class Meta:
+        db_table = "telegram_messages"
+        indexes = [
+            models.Index(fields=["user", "created_at"]),
+            models.Index(fields=["message_type", "is_delivered"]),
+        ]
