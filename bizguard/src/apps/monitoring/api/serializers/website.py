05155393b2d370e903be5930ec44e462ea25b2fd
Mahdi <mahdighadiriafzal@gmail.com>
2025-07-20 23:11:09 +0330

    add serializer for website and uptime

--- bizguard/src/apps/monitoring/api/serializers/website.py
+++ bizguard/src/apps/monitoring/api/serializers/website.py
@@ -1,16 +1,38 @@
 from rest_framework import serializers
 
-from apps.monitoring.models import UptimeCheck
+from apps.monitoring.models import UptimeCheck, Website
+
+
+class WebSiteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Website
+        fields = [
+            "id",
+            "name",
+            "url",
+            "status" "check_interval",
+            "timeout",
+            "check_ssl",
+            "last_check",
+        ]
 
 
 class UptimeCheckSerializer(serializers.ModelSerializer):
+    website = WebSiteSerializer(read_only=True)
+
     class Meta:
         model = UptimeCheck
         fields = [
             "id",
+            "website",
             "uptime",
             "response_time",
             "status_code",
+            "error_code",
             "error_message",
             "checked_at",
+            "headers",
+            "content_length",
+            "redirect_url",
+            "ip_address",
         ]
